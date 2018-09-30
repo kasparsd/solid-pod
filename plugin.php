1a396ec62a17dd0a21be2d5e2c9b445407e3ad46
Kaspars Dambis <hi@kaspars.net>
2018-09-30 14:09:50 +0300

    Plugin abstraction with autoloading

---
+++ plugin.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Plugin Name: Solid POD
+ * Plugin URI: https://github.com/kasparsd/solid-pod
+ * Description: Solid POD provider for WordPress.
+ * Version: 0.0.1
+ * Author: Kaspars Dambis
+ * Author URI: https://kaspars.net
+ * Text Domain: solid-pod
+ */
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+$plugin = new Plugin( __FILE__ );
