9a7f33abe9f9f254ffdaa8ccbe44b4c84ad23704
Kaspars Dambis <hi@kaspars.net>
2018-09-30 20:19:03 +0300

    Add user profile image

--- src/RdfBuilder.php
+++ src/RdfBuilder.php
@@ -10,6 +10,10 @@ class RdfBuilder {
 		$graph = new Graph();
 		$webid = new WebId( $user );
 
+		$user_gravatar = get_avatar_url( $user->user_email, [
+			'size' => 600,
+		] );
+
 		$author_resource = $graph->resource( $webid->webid_uri(), 'foaf:Person' );
 		$account_resource = $graph->resource( $webid->account_uri(), 'sioc:UserAccount' );
 
@@ -20,6 +24,7 @@ class RdfBuilder {
 		$author_resource->set( 'bio:olb', $user->user_description ?: null );
 		$author_resource->set( 'foaf:account', $account_resource );
 		$author_resource->set( 'foaf:homepage', $user->user_url ? $graph->resource( $user->user_url ) : null );
+		$author_resource->set( 'foaf:img', $graph->resource( $user_gravatar ) );
 
 		$account_resource->set( 'sioc:name', $user->display_name ?: null );
 		$account_resource->set( 'sioc:account_of', $author_resource );
